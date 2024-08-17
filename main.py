03f2dee4ec38e8b65eee53a8e08aa52c32559a75
Zach Dyar <zdyar89@gmail.com>
2024-08-17 10:13:57 +0000

    adding main file to init localhost for youtube api connection

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+from dotenv import load_dotenv
+from os.path import join, dirname
+import googleapiclient.errors
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+youtube_key = os.environ.get("YOUTUBE_API_KEY")
+
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+
+
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "client_secrets.json"
+
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, SCOPES)
+    print(flow.__dict__)
+    credentials = flow.run_local_server()
+
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+    video_ids = []
+    request = youtube.playlistItems().list(
+        part='contentDetails',
+        playlistId='PLy0Eq9J9QpuheNMasuzlpjY3ha9Jr-gDN',
+        maxResults=50
+    )
+
+    while request:
+        response = request.execute()
+
+        for item in response['items']:
+            video_ids.append(item['contentDetails']['videoId'])
+
+        request = youtube.playlistItems().list_next(request, response)
+
+    print(video_ids)
+
+if __name__ == "__main__":
+    main()
